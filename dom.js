ffb341320b792084fcb8ceacfc88e9c44b387dd4
suletha <suletha.gopi@gmail.com>
2023-10-01 17:47:17 +0530

    ddd

--- dom.js
+++ dom.js
@@ -5,10 +5,17 @@
 // headerTitle.textContent = 'sarah'
 // headerTitle.style.borderBottom = 'solid 3px #000'
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items)
-items[1].textContent = 'suletha';
-items[2].style.fontWeight = 'bold';
-items[3].style.backgroundColor = 'yellow';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items)
+// items[1].textContent = 'suletha';
+// items[2].style.fontWeight = 'bold';
+// items[3].style.backgroundColor = 'yellow';
 
+var header = document.querySelector('#main-header')
 
+header.style.borderBottom = 'solid 8px #ccc'
+var input = document.querySelector('input');
+input.value = 'suletha'
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = 'send'
