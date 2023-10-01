ce66fdc36838086e1febd27194048942aae16e1b
suletha <suletha.gopi@gmail.com>
2023-10-01 12:15:35 +0530

    getStarted

---
+++ dom.js
@@ -0,0 +1,6 @@
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle)
+headerTitle.innerText = 'suletha'
+headerTitle.textContent = 'sarah'
+headerTitle.style.borderBottom = 'solid 3px #000'
