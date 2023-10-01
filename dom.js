6e9264042d6a0095b059b5883a7a0f5cfd573d2b
suletha <suletha.gopi@gmail.com>
2023-10-01 12:35:16 +0530

    start

--- dom.js
+++ dom.js
@@ -1,6 +1,14 @@
 //console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-console.log(headerTitle)
-headerTitle.innerText = 'suletha'
-headerTitle.textContent = 'sarah'
-headerTitle.style.borderBottom = 'solid 3px #000'
+// var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle)
+// headerTitle.innerText = 'suletha'
+// headerTitle.textContent = 'sarah'
+// headerTitle.style.borderBottom = 'solid 3px #000'
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items)
+items[1].textContent = 'suletha';
+items[2].style.fontWeight = 'bold';
+items[3].style.backgroundColor = 'yellow';
+
+
