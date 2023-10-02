b9d73b1aa8849e92c5f436f94135800b0a058e61
suletha <suletha.gopi@gmail.com>
2023-10-02 08:24:17 +0530

    ffff

--- dom.js
+++ dom.js
@@ -17,5 +17,40 @@ header.style.borderBottom = 'solid 8px #ccc'
 var input = document.querySelector('input');
 input.value = 'suletha'
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value = 'send'
+//querySelector
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = 'send'
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red'
+
+// var second_item = document.querySelector('.list-group-item:nth-child(2)');
+// second_item.style.color = 'green'
+
+// var last_item = document.querySelector('.list-group-item:last-child');
+// last_item.style.color = 'yellow'
+
+
+//querySelectorAll
+
+var titles = document.querySelectorAll('.title');
+titles[0].textContent = 'Welcome'
+
+// var sec_item = document.querySelectorAll('.list-group-item:nth-child(2)');
+// sec_item.forEach(function(sec_item) {
+//     sec_item.style.color = 'green';
+//   });
+
+
+// var sec_item = document.querySelectorAll('.list-group-item:nth-child(2)');
+// for(var i = 0 ; i < odd.length;i++) {
+//     sec_item.style.color = 'green';
+//   };
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+for(var i = 0 ; i < odd.length;i++){
+    odd[i].style.backgroundColor = 'green'
+    //even[i].style.backgroundColor = '#ccc'
+}
